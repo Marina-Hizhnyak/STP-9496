a35f730ce65d7424362be2b203a7a430a136a692
Dmytro <dm.radchenko.f@gmail.com>
2025-06-28 07:25:53 +0300

    fix faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('[data-faq-box]').forEach(box => {
   box.addEventListener('click', () => {
-    const faqItem = box.closest('.faq-item');
+    const faqItem = box.closest('[data-faq-item]');
     faqItem.classList.toggle('open');
   });
 });
